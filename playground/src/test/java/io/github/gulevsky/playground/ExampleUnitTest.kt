d9c127c820fd1857d431a7738b25dccb70d47b02
Anton Gulevsky <cpu124c41@gmail.com>
2018-11-18 00:45:00 +0300

    initial commit

---
+++ playground/src/test/java/io/github/gulevsky/playground/ExampleUnitTest.kt
@@ -0,0 +1,15 @@
+package io.github.gulevsky.playground
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
+    }
+}
